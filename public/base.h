3c28ddb9cc2cb73d45f8497688ee1d59176504c9
tfewfew <tfewfew@163.com>
2015-12-20 22:00:04 +0800

    【issue】基础头文件 #1 【说明】合入基础头文件

---
+++ public/base.h
@@ -0,0 +1,15 @@
+/***********************************************/
+/* 文件名: base.h                                                          */
+/* 文件说明: 基础类型定义                           */
+/***********************************************/
+
+/* 基础类型宏映射 */
+#define ULONG unsigned int
+#define LONG int
+#define USHORT unsigned short
+#define SHORT short
+#define UCHAR unsigned char
+#define CHAR char
+#define VOID void
+#define NULL_PTR NULL
+
